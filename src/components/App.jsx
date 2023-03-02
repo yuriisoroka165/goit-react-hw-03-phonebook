6a1fb1ea6c938ff20f5f1b87ea7a4e46fd9d6bce
Yurii Soroka <yurisoroka165@gmail.com>
2023-03-02 12:07:19 +0200

    Complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,22 @@ import css from "./App.module.css";
 
 class App extends Component {
     state = {
-        contacts: [
-            { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-            { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-            { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-            { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-            { id: "id-5", name: "Yurii Soroka", number: "111-11-11" },
-        ],
+        contacts: [],
         filter: "",
+    };
+
+    componentDidMount() {
+        const contacts = localStorage.getItem("contacts");
+        const parsedContacts = JSON.parse(contacts);
+        if (parsedContacts) {
+            this.setState({ contacts: parsedContacts });
+        }
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.contacts !== prevState.contacts) {
+            localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+        }
     };
 
     deleteContact = contactId => {
